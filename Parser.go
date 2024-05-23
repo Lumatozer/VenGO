6aa28690c4029fb219d58d56d671a9bf00fd3f02
aludayalu <pl46u3.ducklinz@gmail.com>
2024-05-24 01:22:04 +0530

    core created

--- Parser.go
+++ Parser.go
@@ -1,47 +1,42 @@
 package main
 
-// no type validation for instructions
-
 type Object struct {
-	Type 			    *Type
+	Type 			    []string
 	Location        	int
 	Callable_Functions  []int
-	Int_Mapping         map[int]int
-	Int64_Mapping       map[int64]int
-	Float64_Mapping     map[float32]int // uses the same space as a pointer when uninitialised (8 bytes on 64 bit)
-	Float_Mapping       map[float64]int
-	Field_Children      map[int]int
-	Children            []int // index in scope objects to allow scope based object localisation
-}
-
-type Type struct {
-	Name				string
-	Is_Array            bool
-	Is_Dict             bool
-	Is_Primitive        bool
-	Primitive_Type      string
-	Map_Fields          map[string]Type
-	Child				*Type
+	Int_Mapping         map[int]*Object
+	Int64_Mapping       map[int64]*Object
+	Float64_Mapping     map[float32]*Object
+	Float_Mapping       map[float64]*Object
+	Field_Children      map[int]*Object
+	Children            []*Object
 }
 
 type Function struct {
 	Id                  int
 	Name 				string
-	Stack_Spec          []int        // initialize these objects with new pointers for this scope
-	Instructions        [][]int      // Instruction set for this function
+	Stack_Spec          []int                             // initialize these objects with default properties with new pointers for this scope
+	Instructions        [][]int                           // Instruction set for this function
+	Arguments           map[string][]string
+	Out_Type            []string
+	Base_Scope          *Scope                            // for initializing a new Function scope under the program this function belongs to. Will always be the Program's Rendered Scope as only the arguments change
 }
 
 type Scope struct {
 	Ip                  int
 	Objects             []*Object
-	// Return_Scope        *Scope not needed as you will anyways recursively grow the scope, it will anyways return to the last scope
 }
 
 type Program struct {
 	Functions			[]*Function
-	Structs             []*Type
+	Structs             map[string]map[string][]string
+	Rendered_Scope      Scope                            // This Scope will be used for initalizing functions of this file + will retain all the final global states of the variables
+	State_Variables     []int                            // Indices of variables to be stored on the blockchain for this program
 }
 
 type Execution struct {
-	
+	Gas_Limit            int64
+	Entry_Program        *Program
+	Entry_Function       *Function
+	Programs             []Program
 }
